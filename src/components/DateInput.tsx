fd2a0d9e4408e97e91645341e98788121d766fe1
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-05-21 20:47:15 -0300

    feat: novas incrementacoes

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -1,16 +1,45 @@
 import React from "react";
 
-interface IDateInput extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
-}
+const generalStyle: React.CSSProperties = {
+  fontSize: "1rem",
+  color: "var(--color-2)",
+  padding: "var(--gap-s) .75rem",
+  backgroundColor: "var(--color-4)",
+  borderRadius: "var(--gap)",
+};
 
-function DateInput({ label, ...props }: IDateInput) {
+const labelStyle: React.CSSProperties = {
+  display: "block",
+  marginBottom: "var(--gap-s)",
+  fontWeight: "600",
+  ...generalStyle,
+};
+
+const inputStyle: React.CSSProperties = {
+  border: "none",
+  fontFamily: "monospace",
+  ...generalStyle,
+};
+
+type IDateInput = React.ComponentProps<"input"> & {
+  label: string;
+};
+
+const DateInput = ({ label, ...props }: IDateInput) => {
   return (
     <div>
-      <label htmlFor={label}>{label}</label>
-      <input id={label} type="date" name={label} {...props} />
+      <label style={labelStyle} htmlFor={label}>
+        {label}
+      </label>
+      <input
+        style={inputStyle}
+        id={label}
+        name={label}
+        type="date"
+        {...props}
+      />
     </div>
   );
-}
+};
 
 export default DateInput;
