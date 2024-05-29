cb07b3684ade703cd142f8e54bbd606802d68d86
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-05-29 08:46:01 -0300

    feat: ajustes em componente mesbtn para ajustar as datas e puxar o mes que queremos

--- src/components/mesBtn.tsx
+++ src/components/mesBtn.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 
+function nomeMeses(n: number) {
+  //pegando data de hoje
+  const date = new Date();
+  //pegando o mes
+  console.log(date.getMonth() + n);
+  //pegando o nome do mes
+  //INTL => objeto de internacionalizacao
+  const nomeMes = new Intl.DateTimeFormat("pt-BR", {
+    month: "long", //para nao abreviar e dar o nome inteiro
+  }).format(date);
+}
+
 function MesBtn({ n }: { n: number }) {
+  nomeMeses(n);
+
   return <div>MesBtn {n}</div>;
 }
 
