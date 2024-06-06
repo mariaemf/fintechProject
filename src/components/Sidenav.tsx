4969468550e7d455c9b0cb43bf6c0dad08a3cca5
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-06-06 20:41:10 -0300

    feat: realizacao de nova sesssao de sidnav alem de novas imagens e estilizacoes adicionadas a aplicacao

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import fintech from "../assets/fintech.svg";
+import resumo from "../assets/icons/resumo.svg";
+import vendas from "../assets/icons/vendas.svg";
+import webHooks from "../assets/icons/webhooks.svg";
+import configuracoes from "../assets/icons/configuracoes.svg";
+import contato from "../assets/icons/contato.svg";
+import sair from "../assets/icons/sair.svg";
 
 function Sidenav() {
-  return <div>Sidenav</div>;
+  return (
+    <nav>
+      <img src={fintech} alt="" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webHooks} alt="" />
+          </span>
+          <a href="">webHooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} alt="" />
+          </span>
+          <a href="">configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} alt="" />
+          </span>
+          <a href="">contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} alt="" />
+          </span>
+          <a href="">sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Sidenav;
